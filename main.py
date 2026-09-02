6fa0645f06552daec42d2c13155bbeff58e9b77f
Usha474 <usharani9880@gmail.com>
2026-09-02 19:59:28 +0530

    changes to main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from src.addition import add
+from src.subtraction import subtract
+from src.multiplication import multiply
+#from src.division import divide
+
 def run():
     print("--- CLI Calculator App Running ---")
-    # Feature modules will be imported and called here as they are developed
+    print(f"Addition (5 + 3): {add(5, 3)}")
+    print(f"Subtraction (10 - 4): {subtract(10, 4)}")
+    print(f"Multiplication (6 * 7): {multiply(6, 7)}")
+   # print(f"Division (20 / 5): {divide(20, 5)}")
 
 if __name__ == "__main__":
     run()
